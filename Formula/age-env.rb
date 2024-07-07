c73db98f5a5544f799157715d59dffcf54b1487e
Adriano <jld.adriano@gmail.com>
2024-07-06 19:58:43 -0700

    Fix URL

--- Formula/age-env.rb
+++ Formula/age-env.rb
@@ -4,7 +4,7 @@
 class AgeEnv < Formula
   desc "Manage your environment files using age"
   homepage "https://github.com/jld-adriano/age-env"
-  url "https://github.com/federico-terzi/espanso/releases/latest/download/age-env.tar.gz"
+  url "https://github.com/jld-adriano/age-env/releases/latest/download/age-env.tar.gz"
   sha256 "58854a2c29cf44a777803ddea814cbddcd15fbb27f8ef643e55158c93f6431f0"
   version "0.1.0"
 
