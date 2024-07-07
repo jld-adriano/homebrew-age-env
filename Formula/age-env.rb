8be7623eea11483ab44b66cecec7de01bab4c033
Adriano <jld.adriano@gmail.com>
2024-07-06 21:34:44 -0700

    Bump version to 0.1.2

--- Formula/age-env.rb
+++ Formula/age-env.rb
@@ -5,8 +5,8 @@ class AgeEnv < Formula
   desc "Manage your environment files using age"
   homepage "https://github.com/jld-adriano/age-env"
   url "https://github.com/jld-adriano/age-env/releases/latest/download/age-env.tar.gz"
-  sha256 "c24b93bd38a546729586e68f70b3cb1e021bd6c75a71060c6605409d9b8e8e2f"
-  version "0.1.1"
+  sha256 "8d41bc5c5cab9e9bbf6d3664d4ed444b3c4f58cd6a07c6dae0478cd534f74c28"
+  version "0.1.2"
 
   def install
     bin.install "age-env"
